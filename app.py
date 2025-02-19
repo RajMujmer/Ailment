d98378e728765a1b1dd1870c4042637b1be9b9cb
RajMujmer <rajat.mujmer0206@gmail.com>
2025-02-19 12:48:12 +0530

    app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # loading the saved models
 
-diabetes_model = pickle.load(open(f'{working_dir}/saved_models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open(f'{working_dir}/diabetes_model.sav', 'rb'))
 
 heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart_disease_model.sav', 'rb'))
 
